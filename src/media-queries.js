861627779901b8b56f885c0ebcebef229c8b2d5a
nerds-are-in <annacantthinkofagoodaddress@hotmail.co.uk>
2022-01-02 19:08:56 +0000

    more responsive styling

--- src/media-queries.js
+++ src/media-queries.js
@@ -5,13 +5,15 @@ const mainBox = document.getElementById("mainBox");
 const sidebar = document.getElementById("lists-sidebar");
 const addItemBox = document.getElementById("addItemBox");
 const widthTrigger = window.matchMedia("screen and (max-width: 750px)");
+const sortBox = document.getElementById("sortBox");
 
 function addMobileStyles(x) {
     if (x.matches) {
         console.log("matched");
         mainBox.classList.add("mobile");
-        sidebar.classList.add("mobile", "hidden");
-        addItemBox.classList.add("mobile", "hidden");
+        sidebar.classList.add("mobile");
+        addItemBox.classList.add("mobile");
+        sortBox.classList.add("mobile");
         const topNavBar = document.createElement("div");
         topNavBar.id = "topNavBar";
         document.body.appendChild(topNavBar);
@@ -19,7 +21,12 @@ function addMobileStyles(x) {
         //topNavBar.appendChild(listTitle);
     }
     else {
-    
+        mainBox.classList.remove("mobile");
+        sidebar.classList.remove("mobile");
+        addItemBox.classList.remove("mobile");
+        sortBox.classList.remove("mobile");
+        document.body.removeChild(topNavBar);
+        document.body.insertBefore(mainTitle, sidebar);
     }
 }
 
