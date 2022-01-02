1ced4af161754d5cb38ca06a884dfe33d205557a
nerds-are-in <annacantthinkofagoodaddress@hotmail.co.uk>
2022-01-02 21:46:11 +0000

    Completed mobile nav bar.

--- src/media-queries.js
+++ src/media-queries.js
@@ -7,24 +7,45 @@ const addItemBox = document.getElementById("addItemBox");
 const widthTrigger = window.matchMedia("screen and (max-width: 750px)");
 const sortBox = document.getElementById("sortBox");
 
+function addMobileClass() {
+    mainBox.classList.add("mobile");
+    sidebar.classList.add("mobile");
+    addItemBox.classList.add("mobile");
+    sortBox.classList.add("mobile");
+}
+
+function removeMobileClass() {
+    mainBox.classList.remove("mobile");
+    sidebar.classList.remove("mobile");
+    addItemBox.classList.remove("mobile");
+    sortBox.classList.remove("mobile");
+}
+
+function createMobileButtons() {
+    const innerNav = document.createElement("nav");
+    const viewListsButton = document.createElement("button");
+    const openAddItemBoxButton = document.createElement("button");
+    viewListsButton.textContent = "View Lists";
+    openAddItemBoxButton.textContent = "Add To-Do Item"
+    innerNav.id = "innerNav";    
+    viewListsButton.className = "mobile-button";  
+    openAddItemBoxButton.className = "mobile-button"; 
+    innerNav.appendChild(viewListsButton);
+    innerNav.appendChild(openAddItemBoxButton);
+    return innerNav;
+}
+
 function addMobileStyles(x) {
     if (x.matches) {
-        console.log("matched");
-        mainBox.classList.add("mobile");
-        sidebar.classList.add("mobile");
-        addItemBox.classList.add("mobile");
-        sortBox.classList.add("mobile");
+        addMobileClass();       
         const topNavBar = document.createElement("div");
         topNavBar.id = "topNavBar";
         document.body.appendChild(topNavBar);
-        topNavBar.appendChild(mainTitle);
-        //topNavBar.appendChild(listTitle);
+        topNavBar.appendChild(mainTitle);       
+        topNavBar.appendChild(createMobileButtons());               
     }
     else {
-        mainBox.classList.remove("mobile");
-        sidebar.classList.remove("mobile");
-        addItemBox.classList.remove("mobile");
-        sortBox.classList.remove("mobile");
+        removeMobileClass();
         document.body.removeChild(topNavBar);
         document.body.insertBefore(mainTitle, sidebar);
     }
